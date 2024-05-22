d20b68897307f55564c4f2ed3b9b719a334104be
Ellie Cohen <elysemc1@gmail.com>
2024-05-22 15:50:22 -0700

    finish all pokemon character attribute tables

--- finaldb.sql
+++ finaldb.sql
@@ -21,6 +21,7 @@ CREATE TABLE Pokemon_Characters (
 CREATE TABLE Pokemon_Types (
     PRIMARY KEY (pokemon_id, type),
     -- CASCADE: if a Pokemon Character is deleted or a Type is deleted, delete this association too
+    -- note that this does NOT mean that deleting a Pokemon Character => deleting its type from Types
     FOREIGN KEY (pokemon_id) REFERENCES Pokemon_Characters(pokemon_id) ON DELETE CASCADE,
     FOREIGN KEY (type) REFERENCES Types(type) ON DELETE CASCADE
 )
@@ -33,11 +34,12 @@ CREATE TABLE Pokemon_Regions (
     FOREIGN KEY (pokemon_id) REFERENCES Pokemon_Characters(pokemon_id) ON DELETE CASCADE
 )
 
--- Create table: Pokemon_Egg_Group
--- link Pokemon to their Egg Groups
-CREATE TABLE Pokemon_Egg_Group (
-    egg_group 
-    PRIMARY KEY (pokemon_id, egg_group)
+-- Create table: Pokemon_Egg_Groups
+-- link Pokemon to their Egg Groups, must be its own table since pokemon may have >=1 egg group
+CREATE TABLE Pokemon_Egg_Groups (
+    egg_group VARCHAR(14) NOT NULL,
+    PRIMARY KEY (pokemon_id, egg_group),
+    FOREIGN KEY (pokemon_id) REFERENCES Pokemon_Characters(pokemon_id) ON DELETE CASCADE
 )
 
 -- Create table: Types
@@ -47,10 +49,43 @@ CREATE TABLE Types (
     type_name VARCHAR(8) PRIMARY KEY
 )
 
+-- Create table: Type_Efficacy
+CREATE TABLE Type_Efficacy (
+    -- this will be 0.0, 0.5, or 2.0
+    type_effect FLOAT NOT NULL, 
+    PRIMARY KEY (attacking_type, defending_type),
+    -- CASCADE: if we delete a type, we delete its efficacy association(s)
+    FOREIGN KEY (attacking_type) REFERENCES Types (type_name) ON DELETE CASCADE
+    FOREIGN KEY (defending_type) REFERENCES Types (type_name) ON DELETE CASCADE
+)
+
 -- Create table: Regions
 CREATE TABLE Regions (
     -- assume region name is <= 50 chars and unique
     region_name VARCHAR(50) PRIMARY KEY
+)
+
+-- Create table: Attacks
+CREATE TABLE Attacks (
+    attack_name VARCHAR(50) PRIMARY KEY,
+    category VARCHAR(8) NOT NULL,
+    -- power is optional
+    power INT,
+    power_points INT NOT NULL,
+    -- accuracy is optional
+    accuracy FLOAT,
+    effect VARCHAR (50)
+    -- CASCADE: if we delete a type, we can't have an attack of that type, so delete the attack
+    FOREIGN KEY (attack_type) REFERENCES Types (type_name) ON DELETE CASCADE
+)
+
+-- Create table: Pokemon_Attacks
+CREATE TABLE Pokemon_Attacks (
+    level_unlocked INT NOT NULL,
+    PRIMARY KEY (pokemon_id, attack_name),
+    -- CASCADE: if we delete a character or an attack, delete the association
+    FOREIGN KEY (pokemon_id) REFERENCES Pokemon_Characters (pokemon_id) ON DELETE CASCADE
+    FOREIGN KEY (attack_name) REFERENCES Attacks (attack_name) ON DELETE CASCADE
 )
 
 -- Create table: EV_Yield
@@ -68,7 +103,6 @@ CREATE TABLE Evolutions (
     FOREIGN KEY (evolved_id) REFERENCES Pokemon_Characters(pokemon_id) ON DELETE RESTRICT,
     FOREIGN KEY (original_id) REFERENCES Pokemon_Characters(pokemon_id) ON DELETE RESTRICT
 )
-
 
 ------------------- 2. User and Team Associations -------------------
 -- Create table: Favorites
